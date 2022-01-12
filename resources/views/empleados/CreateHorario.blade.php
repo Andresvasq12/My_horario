0fec011214697ca6bcdf816fe66e011400183f78
Andresvasq12 <jordan.vasquez@est.iudigital.edu.co>
2022-01-12 12:22:52 -0500

    change route

--- resources/views/empleados/CreateHorario.blade.php
+++ resources/views/empleados/CreateHorario.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <a class="btn btn-primary ms-3" href="{{url('home')}}">Volver</a>
-<form action="{{url('empleado.CreateHorario'.$id)}}" method="post">
+<form action="{{url('empleado.CreateHorario1'.$id)}}" method="post">
 
 @csrf
 
